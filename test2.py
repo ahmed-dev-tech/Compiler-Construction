47c4f2decb5176d08a6d8662d485f5680a88135c
ahmed-dev-tech <ahmedshahid0335@gmail.com>
2021-11-02 12:34:10 +0500

    token writer
    token writer with token generator

--- test2.py
+++ test2.py
@@ -1,3 +1,94 @@
+# import re
+
+# keyword = {"if":"if","switch":"switch","choice":"choice","crack":"crack","default":"default","although":"although",
+#         "then":"then","otherwise":"otherwise","subject":"subject","retaliate":"retaliate",
+#           "digit":"DT","point":"DT","char":"DT","word":"DT","bool":"DT","function":"function","implicit":"implicit", 
+#           'new': 'new','array':'array','continue':'continue',"none":"none","inherit":"inherit", "public":"public", "private":"private",
+#           "protected":"protected", "self":"self", "super":"super"}
+
+# opr = {"+":"PM","-":"PM","*":"MDM","/":"MDM","%":"MDM","!":"NOT","~":"NOT","&&":"&&","||":"||","and":"and","or":"or",
+#       "<":"RO",">":"RO","<=":"RO",">=":"RO","!=":"RO","==":"RO","=":"Assign","<<":"shiftOp",">>":"shiftOp","+=":"AssignOp",
+#        "*=":"AssignOp","/=":"AssignOp","%==":"AssignOp","<<=":"AssignOp",">>=":"AssignOp","++":"IncDec","--":"IncDec",
+#       "**":"Power"}
+      
+# punc = {",":"," , ";":";" , ":":":" , ".":"." , "{":"{" , "}":"}" , "(":"(" , ")":")" ,
+#         "[":"[" , "]":"]" }
+# #12
+# tokens = []
+# # source_code ='digit result = 100;\ndigit result = 150;\ndigit result = 200;\ndigit result = 300;\ndigit result = 400;\n'.splitlines()
+# # for i,word in enumerate(source_code):
+
+# with open("C:/Users/hh/Desktop/CC/program.txt", 'r') as f:
+#     source_code = f.read().splitlines()
+#     f.close()
+
+# def split(string, delimiters=' \t\n'):
+#     result = []
+#     word = ''
+#     for c in string:
+#         if c not in delimiters:
+#             word += c
+#         elif word:
+#             result.append(word)
+#             word = ''
+
+#     if word:
+#         result.append(word)
+
+#     return result
+
+# def listToString(s): 
+#     # initialize an empty string 
+#     str1 = " " 
+#     # return string  
+#     return (str1.join(s)) 
+# c=0
+
+# for word in source_code:
+#     c+=1
+#     word = split(word)
+#     word = listToString(word)
+#     for i in split(word):
+#         if i in keyword.keys(): 
+#             tokens.append(['DATATYPE', i,c])
+#         # This will look for an identifier which would be just a word
+#         # elif re.match("[a-z]", i or re.match("[A-Z]", i)):
+#         #     tokens.append(['IDENTIFIER', i ,c])
+#         # This will look for an operator
+#         elif i in opr.keys():
+#             tokens.append(['OPERATOR', i ,c ])
+#         elif i in punc.keys():
+#             tokens.append(['PUNTUATER', i ,c ])
+#         # This will look for integer items and cast them as a number 
+#         elif re.match(r'^_+[a-zA-Z_0-9]*[A-Za-z0-9]$|^[A-Za-z]+[A-Za-z_0-9]*[A-Za-z0-9]$|^[A-Za-z]+$',i):
+#             tokens.append(["IDENTIFIER", i , c])
+#         elif i[len(i) - 1] == ';':
+#             if re.match(r"\d+\.\d+",i):
+#                 tokens.append(["Float", i , c])
+#             elif re.match(r'\d\d\d',i):
+#                 tokens.append(["INTEGER", i , c])
+#             elif re.match(r'[\w\W\s]?',i):
+#                 tokens.append(["CHARACTER", i , c])
+#             elif re.match(r'[\w\W\s]*',i):
+#                 tokens.append(["STRING", i , c])
+#             else: 
+#                 tokens.append(["CONSTANT", i[:-1],c])
+#         else:
+#             tokens.append(["INVALID_TOKEN", i , c])
+
+# # with open("E:/Compiler-Construction/token.txt",'w') as w:
+# #     w.write(tokens)
+# #     w.close()
+
+
+
+
+
+# print(c)
+# print(tokens)
+# # with open("program.txt", 'r') as f:
+# #     lines = f.read().replace('/n','')
+
 import re
 
 keyword = {"if":"if","switch":"switch","choice":"choice","crack":"crack","default":"default","although":"although",
@@ -13,7 +104,7 @@ opr = {"+":"PM","-":"PM","*":"MDM","/":"MDM","%":"MDM","!":"NOT","~":"NOT","&&":
       
 punc = {",":"," , ";":";" , ":":":" , ".":"." , "{":"{" , "}":"}" , "(":"(" , ")":")" ,
         "[":"[" , "]":"]" }
-#12
+
 tokens = []
 # source_code ='digit result = 100;\ndigit result = 150;\ndigit result = 200;\ndigit result = 300;\ndigit result = 400;\n'.splitlines()
 # for i,word in enumerate(source_code):
@@ -49,38 +140,42 @@ for word in source_code:
     word = listToString(word)
     for i in split(word):
         if i in keyword.keys(): 
-            tokens.append(['DATATYPE', i,c])
+            tokens.append(['DATATYPE', i,str(c)])
         # This will look for an identifier which would be just a word
         # elif re.match("[a-z]", i or re.match("[A-Z]", i)):
         #     tokens.append(['IDENTIFIER', i ,c])
         # This will look for an operator
         elif i in opr.keys():
-            tokens.append(['OPERATOR', i ,c ])
+            tokens.append(['OPERATOR', i , str(c)])
         elif i in punc.keys():
-            tokens.append(['PUNTUATER', i ,c ])
+            tokens.append(['PUNTUATER', i ,str(c)])
         # This will look for integer items and cast them as a number 
         elif re.match(r'^_+[a-zA-Z_0-9]*[A-Za-z0-9]$|^[A-Za-z]+[A-Za-z_0-9]*[A-Za-z0-9]$|^[A-Za-z]+$',i):
-            tokens.append(["IDENTIFIER", i , c])
+            tokens.append(["IDENTIFIER", i , str(c)])
         
         elif re.match(r"^'[a-zA-Z0-9]'$|^'[!@#$%^&*()-=+{}|;:<>,.?/']'$|^'\\[\'\"\\]'$|^'\\[ntrafb0]'$",i):
-            tokens.append(["CHARACTER", i , c])
+            tokens.append(["CHARACTER", i , str(c)])
 
         elif i[len(i) - 1] == ';':
             if re.match(r"\d+\.\d+",i):
-                tokens.append(["Float", i , c])
+                tokens.append(["Float", i , str(c)])
             elif re.match(r'\d\d\d',i):
-                tokens.append(["INTEGER", i , c])
+                tokens.append(["INTEGER", i , str(c)])
             elif re.match(r'[\w\W\s]?',i):
-                tokens.append(["CHARACTER", i , c])
+                tokens.append(["CHARACTER", i , str(c)])
             elif re.match(r'[\w\W\s]*',i):
-                tokens.append(["STRING", i , c])
+                tokens.append(["STRING", i , str(c)])
             else: 
-                tokens.append(["CONSTANT", i[:-1],c])
+                tokens.append(["CONSTANT", i[:-1],str(c)])
         else:
-            tokens.append(["INVALID_TOKEN", i , c]) 
-                
-
-print(c)
-print(tokens)
-# with open("program.txt", 'r') as f:
-#     lines = f.read().replace('/n','')
+            tokens.append(["INVALID_TOKEN", i , str(c)]) 
+t = []
+for i in tokens:
+    for j in i:
+        t.append(j)
+a=listToString(t)
+b=a.replace(' ',',')
+print(b)
+f = open("token.txt", "w")
+f.write(b)
+f.close()                
